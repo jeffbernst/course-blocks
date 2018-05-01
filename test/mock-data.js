6a97dad0eb4c26b54e92eda5d62e1e3f095fe6fb
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-01 15:49:29 -0400

    add beginning of tests for courses

--- test/mock-data.js
+++ test/mock-data.js
@@ -33,86 +33,11 @@ const mockCourseDataUpdated = {
 const mockUserData = {
   userName: 'Jeff',
   gravatarHash: '75ad827dc5ac6baa1df806dfe15b394e',
-  enrolledIn: [
-    {
-      currentLesson: 5,
-      currentPart: 3,
-      completed: [[1, 2]],
-      // should courseData be linked from courses database?
-      courseData: {
-        courseTitle: 'My Great Course',
-        themeColor: 'purple',
-        tags: [],
-        courseSummary: 'My great summary.',
-        lessons: [
-          {
-            lessonTitle: 'My Great Lesson',
-            parts: [
-              {
-                partTitle: 'My Great Part',
-                partContent: 'Text goes here.'
-              }
-            ]
-          }
-        ]
-      }
-    }
-  ],
+  enrolledIn: [],
   drafts: [
     {
       courseTitle: 'My Great Course',
       themeColor: 'purple',
-      tags: [],
-      courseSummary: 'My great summary.',
-      lessons: [
-        {
-          lessonTitle: 'My Great Lesson',
-          parts: [
-            {
-              partTitle: 'My Great Part',
-              partContent: 'Text goes here.'
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-
-const mockUserDataUpdated = {
-  userName: 'Jeff',
-  gravatarHash: '75ad827dc5ac6baa1df806dfe15b394e',
-  enrolledIn: [
-    {
-      currentLesson: 5,
-      currentPart: 3,
-      completed: [[1, 2]],
-      // should courseData be linked from courses database?
-      courseData: {
-        courseTitle: 'My Great Course',
-        themeColor: 'purple',
-        tags: [],
-        courseSummary: 'My great summary.',
-        lessons: [
-          {
-            lessonTitle: 'My Great Lesson',
-            parts: [
-              {
-                partTitle: 'My Great Part',
-                partContent: 'Text goes here.'
-              }
-            ]
-          }
-        ]
-      }
-    }
-  ],
-  drafts: [
-    {
-      courseTitle: 'My Great Course Was Just UPDATED!!!!!',
-      themeColor: 'purple',
-      tags: [],
-      courseSummary: 'UPDATED!!',
       lessons: [
         {
           lessonTitle: 'My Great Lesson',
@@ -138,6 +63,5 @@ module.exports = {
   mockCourseData,
   mockCourseDataUpdated,
   mockUserData,
-  mockUserDataUpdated,
   mockSignupData,
 }
--- test/test-courses.js
+++ test/test-courses.js
@@ -1,51 +1,78 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const {app, runServer, closeServer} = require('../server')
-const {publishCourse} = require('../routes/coursesRouter')
-const {
-  createNewDraftAndUpdateUser,
-  updateDraftInUserObject
-} = require('../routes/draftsRouter')
-const {createNewUser, getUser} = require('../routes/usersRouter')
-const {Course} = require('../models/course')
-const {User} = require('../models/user')
-const chaiSubset = require('chai-subset')
-const {TEST_DATABASE_URL, PORT} = require('../config')
-const {mockSignupData} = require('./mock-data')
-
-chai.use(chaiSubset)
-chai.use(require('chai-things'))
-
-chai.should()
-
-chai.use(chaiHttp)
-
-async function tearDownDb () {
-  console.warn('Deleting database')
-  await User.remove({})
-  await Course.remove({})
-}
-
-describe('login, signup, and check authentication', () => {
-  before(() => {
-    return runServer(TEST_DATABASE_URL)
-  })
-
-  after(() => {
-    tearDownDb()
-    return closeServer()
-  })
-
-  // - publish course
-  // - get 12 courses for index page
-  // - get specified course
-  // - search courses
-  // - enroll in course
-  // - mark part of course complete
-
-  let token
-
-  it('should publish course', () => {
-
-  })
-})
+// const chai = require('chai')
+// const chaiHttp = require('chai-http')
+// const {app, runServer, closeServer} = require('../server')
+// const {publishCourse} = require('../routes/coursesRouter')
+// const {
+//   createNewDraftAndUpdateUser,
+//   updateDraftInUserObject
+// } = require('../routes/draftsRouter')
+// const {createNewUser, getUser} = require('../routes/usersRouter')
+// const {Course} = require('../models/course')
+// const {User} = require('../models/user')
+// const chaiSubset = require('chai-subset')
+// const {TEST_DATABASE_URL, PORT} = require('../config')
+// const {mockSignupData, mockCourseData, mockCourseDataUpdated} = require('./mock-data')
+//
+// chai.use(chaiSubset)
+// chai.use(require('chai-things'))
+//
+// chai.should()
+//
+// chai.use(chaiHttp)
+//
+// async function tearDownDb () {
+//   console.warn('Deleting database')
+//   await User.remove({})
+//   await Course.remove({})
+// }
+//
+// describe('login, signup, and check authentication', () => {
+//   before(() => {
+//     return runServer(TEST_DATABASE_URL)
+//   })
+//
+//   after(() => {
+//     tearDownDb()
+//     return closeServer()
+//   })
+//
+//   let token
+//
+//   // these two tests just used to set up user and authentication
+//
+//   it('should sign up a user for drafts', () => {
+//     return chai.request(app)
+//       .post('/api/users/')
+//       .send(mockSignupData)
+//       .then(response => {
+//         response.should.have.status(201)
+//         response.body.should.have.property('authToken')
+//       })
+//   })
+//
+//   it('should login and return a token for drafts', async () => {
+//     const hashedPassword = await User.hashPassword(mockSignupData.password)
+//     await User.create({
+//       userName: mockSignupData.name,
+//       userEmail: mockSignupData.email,
+//       password: hashedPassword
+//     })
+//
+//     return chai.request(app)
+//       .post('/api/users/login')
+//       .send({
+//         userEmail: mockSignupData.email,
+//         password: mockSignupData.password
+//       })
+//       .then(response => {
+//         response.should.have.status(200)
+//         response.body.should.have.property('authToken')
+//
+//         token = response.body.authToken
+//       })
+//   })
+//
+//   // course tests below here
+//
+//
+// })
